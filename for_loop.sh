e1ebd4464251f65d6a0eccd17aa563322777035c
Rahul Kumar <95223485+RahulSinha9@users.noreply.github.com>
2024-03-09 10:48:31 +0530

    Add files via upload

---
+++ for_loop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This is for
+
+<< comment
+ 1 is argument 1 which is folder name
+ 2 is start range
+ 3 is end rangeask
+
+comment
+
+for (( num=$2 ; num<=$3; num++ ))
+do
+	mkdir "$1$num"
+done
